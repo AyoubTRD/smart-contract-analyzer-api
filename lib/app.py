f09499abbc64c3526ea4a37bab7a5f4d9d117736
Ayoub Taouarda <ayoubtrdone@gmail.com>
2024-05-20 21:36:35 +0100

    Add CORS allow *

--- lib/app.py
+++ lib/app.py
@@ -1,22 +1,24 @@
 from flask import Flask, request
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 
 from lib.models.model_service import modelService
 
 app = Flask(__name__)
 
-CORS(app, resources={r"/*": {"origins": "*"}})
-
+CORS(app)
 
 @app.route("/")
+@cross_origin()
 def home():
     return "<p>Hello world</p>"
 
 @app.route('/models')
-def get_avialable_models():
+@cross_origin()
+def get_available_models():
     return [model.toDict() for model in modelService.get_available_models()]
 
 @app.route('/analyze/sourcecode')
+@cross_origin()
 def analyze_sourcecode():
     modelId = request.args.get('modelId')
     sourcecode = request.args.get('sourcecode')
@@ -29,6 +31,7 @@ def analyze_sourcecode():
     return model.analyze(sourcecode).toDict()
 
 @app.route('/analyze/bytecode')
+@cross_origin()
 def analyze_bytecode():
     modelId = request.args.get('modelId')
     bytecode = request.args.get('bytecode')
